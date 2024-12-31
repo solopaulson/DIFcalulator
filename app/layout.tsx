875b77d27ad82e270d686ae181723e72c8988139
Solo Paulson <jacobjpbryant@gmail.com>
2024-12-31 13:29:31 -0700

    Update site title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DIFcalculator",
-  description: "Calculate your DIF score",
+  title: "DIF Calculator",
+  description: "Calculate DIF scores easily",
 };
 
 export default function RootLayout({
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
